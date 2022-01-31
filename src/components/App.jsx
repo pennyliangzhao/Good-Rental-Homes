00fc9e40a58d220747af0f200cfd62013a37c358
Yuriliu0909 <liuziyu99@gmail.com>
2022-01-31 15:24:22 +1300

    change the questionnaire layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import ToDoItem from "./ToDoItem";
-import InputArea from "./InputArea";
 import CreateArea from "./CreateArea";
+import DefaultNotes from "../notes";
 
 function App() {
     const [notes, setNotes] = useState([]);
@@ -25,78 +24,40 @@ function App() {
 
     return (
         <div>
-            <Header />
-            <div className="questionnaire">
-                <p>1.Communication -keeps Tenants informed when needed.How well did they keep you informed of any activities/visits?</p>
-            </div>
-            <div>
-                <p>2.Discretion-  respects tenants privacyHow well did they respect your privacy?</p>
-            </div>
-            <div>
-                <p>3.Responsive-  actioned requests in a timely manner.</p>
-            </div>
-            <div>
-                <p>4.Compliance.complies with NZ Tenancy Act</p>
-            </div>
-            <div>
-                <p>5.Overall rating(score)</p>
-            </div>
-            <CreateArea onAdd={addNote} />
-            {notes.map((noteItem, index) => {
-                return (
+            <Header/>
+            <div className="defaultNotes">
+                {DefaultNotes.map(item => (
                     <Note
-                        key={index}
-                        id={index}
-                        title={noteItem.title}
-                        content={noteItem.content}
-                        onDelete={deleteNote}
-                    />
-                );
-            })}
-            <div>
-                <p>Contact me:- the tenant may choose to offer to beavailable to chat with othertenants thinking of renting from this landlord via a convenient messaging service.Or phone. see more below</p>
+                        key={item.key}
+                        title={item.title}
+                        content={item.content}
+                        star={deleteNote}
+                    />))}
             </div>
-            <Footer />
+            <div className="CreatArea">
+                <CreateArea onAdd={addNote}/>
+                {notes.map((noteItem, index) => {
+                    return (
+                        <div>
+                            <Note
+                                key={index}
+                                id={index}
+                                title={noteItem.title}
+                                content={noteItem.content}
+                                onDelete={deleteNote}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="contactMe">
+                <p>Contact me:- the tenant may choose to offer to beavailable to chat with othertenants thinking of
+                    renting from this landlord via a convenient messaging service.Or phone. see more below</p>
+            </div>
+            <Footer/>
         </div>
     );
 
-
-// const [items, setItems] = useState([]);
-//
-// function addItem(inputText) {
-//     setItems(prevItems => {
-//         return [...prevItems, inputText];
-//     });
-// }
-//
-// function deleteItem(id) {
-//     setItems(prevItems => {
-//         return prevItems.filter((item, index) => {
-//             return index !== id;
-//         });
-//     });
-// }
-//
-// return (
-//     <div className="container">
-//         <div className="heading">
-//             <h1>Comments</h1>
-//         </div>
-//         <InputArea onAdd={addItem} />
-//         <div>
-//             <ul>
-//                 {items.map((todoItem, index) => (
-//                     <ToDoItem
-//                         key={index}
-//                         id={index}
-//                         text={todoItem}
-//                         onChecked={deleteItem}
-//                     />
-//                 ))}
-//             </ul>
-//         </div>
-//     </div>
-// );
 }
 
 
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
+import notes from "../notes";
+import {Stars, StarSharp} from "@material-ui/icons";
 
 function Note(props) {
     function handleClick() {
         props.onDelete(props.id);
     }
 
-
     return (
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
             <button onClick={handleClick}>
-                <DeleteIcon />
+                <StarSharp />
+            </button>
+            <button onClick={handleClick}>
+                <StarSharp />
+            </button>
+            <button onClick={handleClick}>
+                <StarSharp />
+            </button>
+            <button onClick={handleClick}>
+                <StarSharp />
+            </button>
+            <button onClick={handleClick}>
+                <StarSharp />
             </button>
         </div>
     );
