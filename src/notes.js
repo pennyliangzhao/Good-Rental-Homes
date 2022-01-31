e795a865afe173fd71604c47dd7c0f45b3d5be5a
Charles Wise <cmorgenstern@users.noreply.github.com>
2022-01-31 15:49:36 +1300

    change the questionnaire layout (#2)
    Co-authored-by: Yuriliu0909 <liuziyu99@gmail.com>

---
+++ src/notes.js
@@ -0,0 +1,33 @@
+const notes = [
+    {
+        key: 1,
+        title: "Communication",
+        content:
+            "Q1.keeps Tenants informed when needed.How well did they keep you informed of any activities/visits?"
+    },
+    {
+        key: 2,
+        title: "Discretion",
+        content:
+            "Q2.respects tenants privacyHow well did they respect your privacy?"
+    },
+    {
+        key: 3,
+        title: "Responsive",
+        content:
+            "Q3.actioned requests in a timely manner."
+    },
+    {
+        key: 4,
+        title: "Compliance.",
+        content:
+            "Q4.complies with NZ Tenancy Act"
+    },
+    {
+        key: 5,
+        title: "Overall rating(score)",
+        content: ""
+    }
+];
+
+export default notes;
