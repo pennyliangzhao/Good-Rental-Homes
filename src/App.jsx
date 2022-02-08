8cc9d4eade1b77316b5ef0c391304dc07b9bc80b
Charles Wise <charles@charlesbwise.com>
2022-02-09 02:27:09 +1300

    Comment out unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landlords from './components/pages/Landlords';
-import Tenants from './components/pages/Tenants';
 import SignIn from './components/pages/SignIn';
 import RateLandlord from "./components/pages/RateLandlord";
-import RateAtHome from "./components/pages/RateAtHome";
-import RateHouse from "./components/pages/RateHouse";
 import RateTenant from "./components/pages/RateTenant";
-import RateNeighbour from "./components/pages/RateNeighbour";
+//import RateAtHome from "./components/pages/RateAtHome";
+//import RateHouse from "./components/pages/RateHouse";
+//import RateNeighbour from "./components/pages/RateNeighbour";
 import Register from "./components/pages/Register";
 
 function App() {
