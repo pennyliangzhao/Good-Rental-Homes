d9c4c039cc199617935fd80ef7caf362ac861e0f
Bryce-W-Andrews <bryce.w.andrews@gmail.com>
2022-02-09 09:37:20 +1300

    Made improvements to rate my LL layout, still needs work.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import HighlightIcon from "@material-ui/icons/Highlight";
-import "antd/dist/antd.css";
-import { PageHeader, Button } from 'antd';
-
 
 function Header() {
     return (
-        <PageHeader
-            ghost={false}
-            onBack={() => window.history.back()}
-            title="Good Rental Homes"
-            subTitle="Rating the landlord"
-            extra={[
-                <Button key="3">Tenant</Button>,
-                <Button key="2">Tenancy</Button>,
-                <Button key="1" type="primary">
-                    Landlord
-                </Button>,
-            ]}
-        >
-        </PageHeader>
+        <header>
+            <h1>
+                <HighlightIcon />
+                Rate Your Landlord
+            </h1>
+        </header>
     );
 }
 
