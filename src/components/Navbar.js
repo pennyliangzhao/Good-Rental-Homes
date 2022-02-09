465b54a190649dba97f1296b8cd22f36b66d4090
zhaolian2 <Fanny050674358!>
2022-02-09 21:50:35 +1300

    Connected the 5 pages with cards and navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,7 +46,7 @@ function Navbar() {
                         <li className='nav-item'>
 
                             <Link
-                                to='/tenants'
+                                to='/rate-tenant'
                                 className='nav-links'
                                 onClick={closeMobileMenu}
                             >
@@ -55,7 +55,7 @@ function Navbar() {
                         </li>
                         <li className='nav-item'>
                             <Link
-                                to='/landlords'
+                                to='/rate-landlord'
                                 className='nav-links'
                                 onClick={closeMobileMenu}
                             >
