2715cca4309f33c264ec61cd3877e9322aeba7d3
zhaolian2 <Fanny050674358!>
2022-02-11 12:31:14 +1300

    Added social media icons to footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,16 @@
     <title>Document</title>
     <link rel="stylesheet" href="../css/styles.css" />
     <script src="../js/app.js" defer></script>
+    <link
+        href="https://fonts.googleapis.com/css?family=McLaren|Montserrat&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="styles.css" />
+    <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.13.1/css/all.css"
+        integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q"
+        crossorigin="anonymous"
+    />
 </head>
 <body>
 <div id="ReactApp"></div>
