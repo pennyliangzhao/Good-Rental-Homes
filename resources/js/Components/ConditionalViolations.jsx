9f0219eafb3bb2641613ddf11b207e101a1a46b1
Bryce-W-Andrews <bryce.w.andrews@gmail.com>
2022-02-15 19:12:41 +1300

    fixed spelling of 'subletters'

--- resources/js/Components/ConditionalViolations.jsx
+++ resources/js/Components/ConditionalViolations.jsx
@@ -8,7 +8,7 @@ function CheckboxController() {
             <input type="checkbox" onClick={() => setIsToggled(!isToggled)}/>
             { isToggled &&
                 <textarea>
-                    e.g. unauthorized pets/ co-dwellers/ subletting
+                    e.g. unauthorized pets/ co-dwellers/ subletters
                 </textarea>}
 
         </div>
